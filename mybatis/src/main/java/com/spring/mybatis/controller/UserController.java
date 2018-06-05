36d34eefdc14c611aa63d6cc93b8768677e50256
714330240@qq.com <714330240@qq.com>
2018-06-05 17:52:18 +0800

    Update

--- mybatis/src/main/java/com/spring/mybatis/controller/UserController.java
+++ mybatis/src/main/java/com/spring/mybatis/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.spring.mybatis.controller;
 
 import com.spring.mybatis.model.User;
-import com.spring.mybatis.service.UserService;
 import com.spring.mybatis.service.impl.UserServiceImpl;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 /**
  * @ClassName UserController
@@ -37,8 +34,52 @@ public class UserController {
         user.setPassword(password);
         user.setEmail(email);
         userService.insertUser(user);
-        data.put("code","1");
+        data.put("code","0");
         data.put("message","添加成功！");
+        return data;
+    }
+
+    @GetMapping(value = "/findByUserName")
+    @ResponseBody
+    public  Object findByUserName(@Param("userName")String userName){
+        Map<String,Object> data = new HashMap<>();
+        User user = userService.findByUserName(userName);
+        if (null != user) {
+            data.put("user",user);
+            data.put("code","0");
+            data.put("message","查询成功！");
+        } else {
+            data.put("code","0");
+            data.put("message","没有找到用户！");
+        }
+        return data;
+    }
+
+    @GetMapping(value = "/deleteUserById")
+    @ResponseBody
+    public Object deleteUserById(@Param("id")Long id){
+        Map<String,Object> data = new HashMap<>();
+        userService.deleteUserById(id);
+        data.put("code","0");
+        data.put("message","删除成功！");
+        return data;
+    }
+
+    @GetMapping(value = "/updatePassword")
+    @ResponseBody
+    public Object updatePassword(@Param("password")String password,@Param("userName")String userName){
+        Map<String,Object> data = new HashMap<>();
+        User user = userService.findByUserName(userName);
+        if (null != user) {
+            user.setPassword(password);
+            userService.updatePassword(user);
+            data.put("user",user);
+            data.put("code","0");
+            data.put("message","修改成功！");
+        } else {
+            data.put("code","0");
+            data.put("message","没有找到用户！");
+        }
         return data;
     }
 
